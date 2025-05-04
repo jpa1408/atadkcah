818b7d61c52dcae9570d736b15fad3e56dc71aa6
jpa1408 <jpa1408@gmail.com>
2025-05-04 13:51:33 -0500

    Correcciones 26

--- models/silver/staging/stg_github_push_events.sql
+++ models/silver/staging/stg_github_push_events.sql
@@ -44,24 +44,43 @@ WITH source_data AS (
 )
 
 SELECT
-  *,
-  ARRAY_SIZE(push_commits_array) AS number_of_commits,
+  sd.event_id,
+  sd.event_type,
+  sd.push_is_public_event,
+  sd.push_event_created_at,
+  sd.push_user_id,
+  sd.push_user_login,
+  sd.push_user_display_login,
+  sd.push_user_avatar_url,
+  sd.push_repo_id,
+  sd.push_repo_owner,
+  sd.push_repo_name,
+  sd.push_id,
+  sd.push_repository_id,
+  sd.push_git_reference,
+  sd.push_branch_name,
+  sd.push_head_commit_sha,
+  sd.push_before_commit_sha,
+  sd.push_total_commits,
+  sd.push_distinct_commits,
+  sd.push_commits_array,
+  ARRAY_SIZE(sd.push_commits_array) AS number_of_commits,
   ARRAY_AGG(
     OBJECT_CONSTRUCT(
-      'sha', TRY_CAST(commit:sha::STRING AS VARCHAR),
-      'message', TRY_CAST(commit:message::STRING AS VARCHAR),
-      'author_name', TRY_CAST(commit:author:name::STRING AS VARCHAR),
-      'author_email', TRY_CAST(commit:author:email::STRING AS VARCHAR),
-      'distinct', TRY_CAST(commit:distinct::BOOLEAN AS BOOLEAN),
-      'url', TRY_CAST(commit:url::STRING AS VARCHAR)
+      'sha', TRY_CAST(f.commit:sha::STRING AS VARCHAR),
+      'message', TRY_CAST(f.commit:message::STRING AS VARCHAR),
+      'author_name', TRY_CAST(f.commit:author:name::STRING AS VARCHAR),
+      'author_email', TRY_CAST(f.commit:author:email::STRING AS VARCHAR),
+      'distinct', TRY_CAST(f.commit:distinct::BOOLEAN AS BOOLEAN),
+      'url', TRY_CAST(f.commit:url::STRING AS VARCHAR)
     )
-  ) WITHIN GROUP (ORDER BY INDEX) AS parsed_commits
-FROM source_data,
-LATERAL FLATTEN(input => push_commits_array) AS f(commit, index)
+  ) WITHIN GROUP (ORDER BY f.index) AS parsed_commits
+FROM source_data sd,
+LATERAL FLATTEN(input => sd.push_commits_array) AS f
 GROUP BY
-  event_id, event_type, push_is_public_event, push_event_created_at,
-  push_user_id, push_user_login, push_user_display_login, push_user_avatar_url,
-  push_repo_id, push_repo_owner, push_repo_name,
-  push_id, push_repository_id, push_git_reference, push_branch_name, 
-  push_head_commit_sha, push_before_commit_sha, push_total_commits, push_distinct_commits,
-  push_commits_array
+  sd.event_id, sd.event_type, sd.push_is_public_event, sd.push_event_created_at,
+  sd.push_user_id, sd.push_user_login, sd.push_user_display_login, sd.push_user_avatar_url,
+  sd.push_repo_id, sd.push_repo_owner, sd.push_repo_name,
+  sd.push_id, sd.push_repository_id, sd.push_git_reference, sd.push_branch_name, 
+  sd.push_head_commit_sha, sd.push_before_commit_sha, sd.push_total_commits, sd.push_distinct_commits,
+  sd.push_commits_array
