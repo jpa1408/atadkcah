6ecf617cb71464e0a135356ed19975fcc0ea1116
jpa1408 <jpa1408@gmail.com>
2025-05-04 14:45:23 -0500

    Correcciones 34

--- models/gold/marts/dev_analytics/repo_health_metrics.sql
+++ models/gold/marts/dev_analytics/repo_health_metrics.sql
@@ -10,8 +10,8 @@ SELECT
   push_repo_name AS repo_name,
   -- Frecuencia de commits
   COUNT(DISTINCT commit_sha) AS total_commits,
-  DATEDIFF(DAY, MIN(push_event_created_at), MAX(push_event_created_at)) AS days_active,
-  total_commits / NULLIF(days_active, 0) AS commits_per_day,
+  GREATEST(1, DATEDIFF(DAY, MIN(push_event_created_at), MAX(push_event_created_at))) AS days_active,
+  total_commits::FLOAT / days_active AS commits_per_day,
   
   -- Calidad de commits
   AVG(LENGTH(commit_message)) AS avg_commit_message_length,
